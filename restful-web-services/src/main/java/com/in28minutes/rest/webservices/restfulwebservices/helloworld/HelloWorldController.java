b7004785345b339f1e23766bf95cc1d4e068b86d
Vidhish Panchal <vidhishp185@gmail.com>
2023-09-30 11:56:54 +0530

    First commit

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,38 @@
+package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
+
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	private MessageSource messageSource;
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
+	
+    @GetMapping(path="/hello-world")
+    public String helloWorld(){
+        return "Hello World";
+    }
+    @GetMapping(path="/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World Bean");
+    }
+
+    @GetMapping(path="/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
+        return new HelloWorldBean("Hello World, " + name);
+    }
+    @GetMapping(path="/hello-world-internationalization")
+    public String helloWorldInternationalized(){
+    	Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale)
+//    	return "Hello World international";
+    }
+}
---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -0,0 +1,49 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+import java.time.LocalDate;
+
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
+public class User {
+    private Integer id;
+    
+    @Size(min=2, message="Name should have atleast 2 characters.")
+    private String name;
+    
+    @Past(message="Birthdate should always be in past.")
+    private LocalDate birthDate;
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public User(Integer id, String name, LocalDate birthDate) {
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
+}
